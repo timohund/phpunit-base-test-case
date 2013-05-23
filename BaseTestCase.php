6d32f94dc61555bf8fe5e6ce08dd5c236f44860f
Michael Klapper <michael.klapper+github@gmail.com>
2013-05-23 13:31:21 +0300

    Update BaseTestCase.php

--- BaseTestCase.php
+++ BaseTestCase.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Michael Klapper
- * Date: 22.05.13
- * Time: 19:30
- * To change this template use File | Settings | File Templates.
- */
 
 namespace Morphodo\Tests;
 
